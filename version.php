07851ab1b52a4cf5ddc21ac92437c4042a184740
sdk2031 <250481129@qq.com>
2026-06-14 01:39:37 +0800

    更新版本号到3.0.0

--- version.php
+++ version.php
@@ -4,14 +4,14 @@
  * 授权信息更新时间: 2026-03-09 09:24:24
  */
 return array(
-    'version' => '2.4.3',
+    'version' => '3.0.0',
     'auth_code' => '',
     'release_time' => '',
     'expire_time' => '',
     'update_expire_time' => '',
     'last_check_time' => time(),
     'last_update_check' => '',
-    'version_name' => 'v2.4.3',
+    'version_name' => 'v3.0.0',
     'version_update_time' => '',
     'app_name' => '巨量号卡',
     'agent_wechat' => '',
